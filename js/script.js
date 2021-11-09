183cd7952fdaed6e9ac6aee8bd5d2637807e9e36
Rocki <user.email>
2021-11-09 22:50:11 +0200

    third commit

--- js/script.js
+++ js/script.js
@@ -26,3 +26,13 @@ closeOverlay.addEventListener('click', () => {
 function switchMenu() {
     menu.classList.toggle('active');
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
